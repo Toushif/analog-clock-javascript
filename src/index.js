1827c0ee9c5e0168560dc00c6a43c71dabc6d993
Toushif <toushif.haq@gmail.com>
2021-10-31 15:47:49 +0530

    Javascript clock initial init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+setInterval(clock, 1000);
+
+const elementSecond = document.querySelector('[toushif-second]')
+const elementMinute = document.querySelector('[toushif-minute]')
+const elementHour = document.querySelector('[toushif-hour]')
+
+function clock() {
+    const date = new Date(),
+        second = date.getSeconds()/60,
+        minute = (second + date.getMinutes())/60,
+        hour = (minute+(date.getHours() > 12 ? date.getHours() - 12 : date.getHours()))/12;
+        
+        render(elementSecond, second)
+        render(elementMinute, minute)
+        render(elementHour, hour)
+}
+
+function render(element, rotation) {
+    element.style.setProperty('--hand-rotate', rotation*360)
+}
+
+clock()
